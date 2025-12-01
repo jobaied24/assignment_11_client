dff91c6d3405383241d956c8bf70aa430b78878d
hero00 <jobaied543@gmail.com>
2025-12-02 00:24:15 +0600

    added navbar

---
+++ src/Router/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import RootLayout from '../Layout/RootLayout';
+import Home from '../Pages/Home/Home';
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        Component:RootLayout,
+        children:[
+            {
+                index:true,
+                Component:Home
+            }
+        ]
+    }
+])
+export default router;
