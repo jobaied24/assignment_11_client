9d1d67fdbf3aee0592073f1a58a4c72b957e5220
hero00 <jobaied543@gmail.com>
2025-12-14 12:30:11 +0600

    delete item and update ui state

--- src/Pages/myItems/ItemList.jsx
+++ src/Pages/myItems/ItemList.jsx
@@ -1,70 +1,106 @@
-import React, { use } from 'react';
+import axios from 'axios';
+import React, { use, useState } from 'react';
 import { Link } from 'react-router';
+import Swal from 'sweetalert2';
 
-const ItemList = ({itemsPromise}) => {
-    const myItems = use(itemsPromise);
-    console.log(myItems);
-    return (
-        <div className='m-12'>
-            <div className="overflow-x-auto">
+const ItemList = ({ itemsPromise }) => {
+  const initialItems = use(itemsPromise);
+  const [myItems,setmyItems] = useState(initialItems);
 
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>
-          <label>
-            No.
-          </label>
-        </th>
-        <th>Category</th>
-        <th>Item Info</th>
-        <th>Date</th>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
+  const handleDeleteItem = id => {
 
-      {/* rows */}
-      {
-        myItems.map((myItem,index)=>
-              <tr key={myItem._id}>
-        <th>
-          <label>
-            {index+1}
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center gap-3">
-            <div>
-              <div className="font-semibold">{myItem.category}</div>
-              <div className="text-sm opacity-50">{myItem.postType}</div>
-            </div>
-          </div>
-        </td>
-        <td>
-          <span className='font-semibold'>
-        {myItem.title}
-          </span>
-  
-          <br />
-          <span className="badge badge-ghost badge-sm">{myItem.location}</span>
-        </td>
-        <td className='font-medium'>{myItem.date}</td>
-        <th className='flex gap-4'>
-          <Link to={`/updateItem/${myItem._id}`} className="btn btn-primary btn-xs text-white">Update</Link>
-          <Link className="btn btn-primary btn-xs text-white">Delete</Link>
-        </th>
-      </tr>
-       )
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios.delete(`http://localhost:3000/deleteItem/${id}`)
+        .then(res=>{
+          console.log(res.data);
+          if(res.data.deletedCount){
+                  Swal.fire({
+          title: "Deleted!",
+          text: "Item has been deleted.",
+          icon: "success"
+        });
+
+        const remainMyItems = myItems.filter(items =>items._id !== id);
+        setmyItems(remainMyItems);
+          }
+
+        })
+
       }
+    });
 
-      
-  </tbody>
-  </table>
-</div>
-        </div>
-    );
+  }
+  console.log(myItems);
+  return (
+    <div className='m-12'>
+      <div className="overflow-x-auto">
+
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  No.
+                </label>
+              </th>
+              <th>Category</th>
+              <th>Item Info</th>
+              <th>Date</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+
+            {/* rows */}
+            {
+              myItems.map((myItem, index) =>
+                <tr key={myItem._id}>
+                  <th>
+                    <label>
+                      {index + 1}
+                    </label>
+                  </th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div>
+                        <div className="font-semibold">{myItem.category}</div>
+                        <div className="text-sm opacity-50">{myItem.postType}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>
+                    <span className='font-semibold'>
+                      {myItem.title}
+                    </span>
+
+                    <br />
+                    <span className="badge badge-ghost badge-sm">{myItem.location}</span>
+                  </td>
+                  <td className='font-medium'>{myItem.date}</td>
+                  <th className='flex gap-4'>
+                    <Link to={`/updateItem/${myItem._id}`} className="btn btn-primary btn-xs text-white">Update</Link>
+                    <Link onClick={()=>handleDeleteItem(myItem._id)} className="btn btn-primary btn-xs text-white">Delete</Link>
+                  </th>
+                </tr>
+              )
+            }
+
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ItemList;
