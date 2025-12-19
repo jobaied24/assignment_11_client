0ad4af5efb6d61f2851f5733730382f79ccc2053
hero00 <jobaied543@gmail.com>
2025-12-19 23:59:44 +0600

    added search functionality

--- src/Pages/Home/AllItems.jsx/AllItems.jsx
+++ src/Pages/Home/AllItems.jsx/AllItems.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, {useEffect, useState } from 'react';
 import ItemCard from '../itemCard';
+import ErrorCard from '../../../error/ErrorCard';
 
 
 const AllItems = () =>{
@@ -16,13 +17,58 @@ useEffect(()=>{
     .catch(error=>{
         console.log(error);
     })
-    },[])
+    },[]);
+
+
+    const handleSearchItems = e =>{
+    e.preventDefault();
+    const search = e.target.search.value;
+    console.log(search);
+
+    axios(`http://localhost:3000/items?title=${search}`)
+    .then(res=>{
+        console.log(res.data);
+        setItems(res.data);
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+
+    }
     
     return (
     <div>
+
+        {/* search */}
+        <form onSubmit={handleSearchItems}>
+            <fieldset className="fieldset  mx-auto my-6 w-1/2 rounded-box  p-4">
+  <div className="join">
+    <input type="text" name='search' className="input w-full join-item" placeholder="Find items by title" />
+    <button className="btn btn-primary text-white join-item">search</button>
+  </div>
+</fieldset>
+        </form>
+
+
+
+{/* items */}
+
+          {
+           items.length===0 && <div className='flex justify-center'>
+           <div className="card my-10 p-10 w-sm bg-gray-600 text-center shadow-sm">
+  <div className="card-body flex flex-col justify-center items-center text-primary">
+    <h2 className="card-title  text-4xl">Sorry!</h2>
+    <h3 className='text-xl font-medium'>There is no items with this title</h3>
+  </div>
+</div>
+           </div>
+          }
+
+
         <div className='grid md:grid-cols-3 grid-cols-1 gap-8 my-16 mx-6'>
+
             {
-                items.map(item=><ItemCard item={item}></ItemCard>)
+                items.map(item=><ItemCard key={item._id} item={item}></ItemCard>)
             }
         </div>
 
